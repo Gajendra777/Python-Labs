67cb8271d7541da2fbe32e7bb7170765383d6e5d
Gajendra Negi <12572640+Gajendra777@users.noreply.github.com>
2020-09-30 13:22:55 +0530

    Python program for Processing and Manipulating Images

--- Projects/ImageProcessing.py
+++ Projects/ImageProcessing.py
@@ -1,31 +1,36 @@
-#! /bin/usr/python
+#!/usr/bin/env python3
 
 from PIL import Image,UnidentifiedImageError
 import os,sys
 
+#For Windows
 dest = os.path.abspath("C:\\Users\\HP\\Downloads\\opt\\icons")
-
+#For Linux
+# dest = os.path.expanduser("/opt/icons") 
 def processImages(image_path,image_name):
     image_size= (128,128)
     infile = image_path
     file_name,_ = os.path.splitext(image_name)
     try:
-        with Image.open(infile) as img:              
-            img.thumbnail(image_size)
-            img.rotate(-90)
+        with Image.open(infile) as img:
+            img = img.resize(image_size)
+            img = img.rotate(270)
             if img.mode != 'RGB':
                 img = img.convert('RGB')
             image_output = os.path.join(dest,file_name)
-            img.save(image_output,"JPEG")
+            img.save(image_output,"jpeg")
     except UnidentifiedImageError:
         pass
     except OSError as err:
         print("cannot convert {} \n Error is {}".format(infile,type(err)))
 
 def main():
+    # For Windows
     src = os.path.abspath("C:\\Users\\HP\\Downloads\\images")
+    #For Linux
+    #src = os.path.expanduser("~/images")
     if not os.path.exists(dest):
-    os.makedirs(dest)
+        os.makedirs(dest)
     try:
         for _,_,file_names in os.walk(src):
             for file_name in file_names:
